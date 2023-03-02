4d1866668f8aecfba9c8099ce723dc2d37cd92d2
Even Torbjørnsen <591118@stud.hvl.no>
2023-03-02 14:34:37 +0100

    Opprettelse av repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Prosjektet undersøker deep learning modeller for å utforske x-ray av overkroppen, for å finne avvik fra en frisk kropp. Skal deretter sette riktig diagnose på avvikene',
+    author='Guttakrutt gruppen - DAT255',
+    license='MIT',
+)
